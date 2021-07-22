9deabbce8861eb7f846bec5b755ac4603434fb1f
haoxiaoxiao <haoxiaoxiao@netjoy.com>
2021-07-22 14:58:06 +0800

    第一次提交taro-demo

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg: '这是一个taro页面3333333'
+    }
+  }
+}
+</script>
